2d3be444c2f3659da1248d409d55eadc00cb68a0
Kazuhiro Terao <drinkingkazu@nevis.columbia.edu>
2016-03-18 02:45:57 -0400

    minor fix, added nudot --kazu

--- configure.sh
+++ configure.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-# If CAFFE_DIR not set, try to guess                                          
+# If CAFFE_DIR not set, try to guess
 if [[ -z $CAFFE_DIR ]]; then
-    # Find the location of this script:                                         
-    me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    # Find the directory one above.                                             
-    export CAFFE_DIR="$( cd "$( dirname "$me" )" && pwd )"
+    export CAFFE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
 
 case `uname -n` in 
+    (nudot)
+	echo Setting up for nudot...
+	ln -sf $CAFFE_DIR/Makefile.config.nudot $CAFFE_DIR/Makefile.config
+	;;
     (wu)
 	echo Setting up for wu...
-	ln -s $CAFFE_DIR/Makefile.config.wu $CAFFE_DIR/Makefile.config
+	if [[ -z $WU_LARBYS_CONFIG ]]; then
+	    source /etc/larbys.sh
+	fi
+	ln -sf $CAFFE_DIR/Makefile.config.wu $CAFFE_DIR/Makefile.config
 	;;
     (*)
 	echo Unknown machine... Using default
-	ln -s $CAFFE_DIR/Makefile.config.example $CAFFE_DIR/Makefile.config
+	ln -sf $CAFFE_DIR/Makefile.config.example $CAFFE_DIR/Makefile.config
 	;;
 esac
 
